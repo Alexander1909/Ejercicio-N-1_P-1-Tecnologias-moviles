10eba95acf8c7acd2fbca9157d387ce62eaa0259
Alexander1909 <jhonathan.corrales@ucsm.edu.pe>
2026-03-27 07:47:33 -0500

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -7,3 +7,9 @@ import android.widget.EditText;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
+public class MainActivity extends AppCompatActivity {
+
+    EditText etNombre, etApellidos, etEdad;
+    Button btnRegistrar;
+    TextView tvResultado;
+
