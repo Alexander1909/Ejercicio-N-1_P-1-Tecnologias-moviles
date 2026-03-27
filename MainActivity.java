a57eaba884c3f1e60ed957cb135dcc5a4392de5a
Alexander1909 <jhonathan.corrales@ucsm.edu.pe>
2026-03-27 07:47:06 -0500

    MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,9 @@
+package com.example.registrodeestudiante;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
